127d5852034c6ea0a73681f3ad0033e1074004a2
lehenkyi-yevhen-ar <meister.ramires@gmail.com>
2025-01-16 17:37:39 +0200

    commit before relocation

--- src/app/components/status-label.tsx
+++ src/app/components/status-label.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import s from './active-label.module.css'
+import clsx from 'clsx';
 
 export enum Status {
   Active = 'Active',
@@ -11,16 +11,24 @@ export enum Status {
 export interface StatusLabelProps {
   children: React.ReactNode;
   status: Status;
+  disabled?: boolean;
 }
 
-export default function StatusLabel({children, status}: StatusLabelProps) {
+export default function StatusLabel({ children, status, disabled }: StatusLabelProps) {
+  console.log(process.env.NEXT_PUBLIC_CRM_SECRET_KEY);
+  
   return (
     <div
-      className={`inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium ${
-        (status === Status.Active && 'text-green-700 bg-green-100') ||
-        (status === Status.NotActive && 'text-red-700 bg-red-100') ||
-        (status === Status.Pending && 'text-orange-700 bg-orange-100') ||
-        (status === Status.Suspended && 'text-blue-700 bg-blue-100')} `}>
+      className={clsx(
+        'inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium',
+        status === Status.Active && 'text-green-700 bg-green-100',
+        status === Status.NotActive && 'text-red-700 bg-red-100',
+        status === Status.Pending && 'text-orange-700 bg-orange-100',
+        status === Status.Suspended && 'text-blue-700 bg-blue-100',
+        {
+          ['opacity-75 cursor-not-allowed']: disabled,
+        },
+      )}>
       <div className='w-1 h-1 mr-2 rounded-full bg-current'></div>
       {children}
       </div>
