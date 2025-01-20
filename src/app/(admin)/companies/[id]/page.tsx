c77fb4454b740350bcd58a3085d96212c4516b9d
lehenkyi-yevhen-ar <meister.ramires@gmail.com>
2025-01-20 15:59:04 +0200

    add error handlers

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,10 +1,14 @@
-import {use} from 'react';
+import { use } from 'react';
 import Header from '@/app/components/header';
-
+import {notFound} from 'next/navigation'
 
 export default function Page({ params }: { params: Promise<{ id: string }>}) {
   const { id } =  use(params);
   
+    const pId = Number.parseInt(id);
+    if (Number.isNaN(pId)) {
+      notFound();
+    }
   return (
     <>
       <Header>Company ({id}) </Header>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import AddCompanyButton from "./components/add-company-button";
+import MagicButton from './components/magic-button'
+
 
 export default function Home() {
   
@@ -6,6 +8,7 @@ export default function Home() {
       <main>
       <h1 className="text-xl">Home page { new Date().toTimeString() }</h1>
       <AddCompanyButton />
+      <MagicButton/>
       </main>
   );
 }
